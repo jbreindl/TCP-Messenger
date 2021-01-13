c6f62f9d70cc78e5c1c09cb9b8ba241341feebd6
Julius Breindl <=>
2021-01-13 08:48:14 -0500

    established data stream for a message to be sent on

--- Client/TCPClient.cs
+++ Client/TCPClient.cs
@@ -10,12 +10,26 @@ public class TCPClient
     {
         try
         {
+            //establish connection with server
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             IPEndPoint endpoint = new IPEndPoint(ip, 8000);
             TcpClient client = new TcpClient();
             Console.WriteLine("Connecting...");
             client.Connect(endpoint);
             Console.WriteLine("Connected.");
+
+            //read a message from the command line
+            Console.WriteLine("\nWrite a Message");
+            string message = Console.ReadLine();
+            message = message.Trim();
+
+            //send a message to the server
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Sent: {0}", message);
+
+
             client.Close();
 
         }
